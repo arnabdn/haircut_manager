05974540d64aa8c0f741b757f187cae1297f0d4e
bassilisk <arnabdebnath095@gmail.com>
2023-09-15 17:41:01 +0600

    adding existing code to git

---
+++ functions.py
@@ -0,0 +1,64 @@
+import csv
+import datetime as dt
+
+records = []
+temp_records = []
+
+def one_liner(cd):
+    final_lst = []
+    tday_tmrw = input("Did you cut you hair today y/n ")
+    if tday_tmrw == "y":
+        date_str = f"{cd.day}-{cd.month}-{cd.year}"
+        weekday = cd.strftime("%A")
+    else:
+        dm = int(input("what date of month was it? "))
+        while dm > cd.day:
+            print("It's not possible choose a date in future. ")
+            dm = int(input("what date of month was it? "))
+        new_cd = dt.date(cd.year,cd.month,dm)
+        date_str = f"{new_cd.day}-{new_cd.month}-{new_cd.year}"
+        weekday = new_cd.strftime("%A")
+    askplace = input("where did you cut your hair from ")
+    cost = input("How much did it cost? ")
+    final_lst += [date_str,askplace,cost,weekday]
+    final_str = ",".join(final_lst)
+    return f"\n{final_str}"
+
+def load_data():
+    global records
+    with open("log.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row == ['Date', 'Place', 'Cost', 'Weekday']:
+                pass
+            else:
+                records += [row]
+        print("Records loaded...")
+
+def show_data():
+    print("  Date      place     cost    weekday")
+    for i in range(len(records)):
+        print(f"{i + 1} {records[i][0]}  {records[i][1]}  {records[i][2]}  {records[i][3]}")
+
+def delete_from_records(turn):
+    global temp_records,records
+    for index, lines in enumerate(records):
+        if index not in range(turn, len(records) + 1):
+            temp_records += [lines]
+    records = temp_records
+    temp_records = []
+
+def delete_untill_records(turn):
+    global temp_records,records
+    for index, lines in enumerate(records):
+        if index not in range(0,turn):
+            temp_records += [lines]
+    records = temp_records
+    temp_records = []
+
+def reconstruct_csv():
+    with open("log.csv", "w") as file:
+        file.write("Date,Place,Cost,Weekday")
+        for record in records:
+            updated_str = ",".join(record)
+            file.write(f"\n{updated_str}")
---
+++ main.py
@@ -0,0 +1,63 @@
+import datetime as dt
+import functions as fn
+
+cd = dt.datetime.now()
+
+records = []
+temp_records = []
+
+fn.load_data()
+
+
+while True:
+    user_op = input("\n" +
+    "+-----------------------+\n" +
+    "|      Main Menu        |\n" +
+    "| What would you like   |\n" +
+    "| to do?                |\n" +
+    "| 1. Add data           |\n" +
+    "| 2. Reset data         |\n" +
+    "| 3. See stats          |\n" +
+    "| 4. Quit               |\n" +
+    "+-----------------------+\n")
+
+    if user_op == "1":
+        with open("log.csv","a") as file:
+                line = fn.one_liner(cd)
+                file.write(line)
+                continue     
+    elif user_op == "2":
+        ops = input("Do you want to delete all the data? y/n ")
+        if ops == "y":
+            with open("log.csv","w") as file:
+                file.write("Date,Place,Cost,Weekday")
+            records = []
+
+        elif ops == "n":
+            while True:
+                option = input("you have two options:\n"
+                    "1. Reset data starting from a date\n"
+                    "2. Reset data untill a date\n")
+                if option == "1":
+                    fn.show_data()
+                    turn = int(input("starting from(exclusive) which entry you want to reset data? answer in numbers\n"))
+                    fn.delete_from_records(turn)
+                    fn.reconstruct_csv()
+                if option == "2":
+                    fn.show_data()
+                    turn = int(input("up until(inclusive) which entry you want to reset data? anwer in numbers\n"))
+                    fn.delete_untill_records(turn)
+                    fn.reconstruct_csv()
+                op = input("Do you want to reset more entry? y/n\n")
+                if op == "y":
+                    continue
+                else:
+                    break
+        
+    elif user_op == "3":
+        pass
+    elif user_op == "4":
+        print("See you next time!")
+        break
+    else:
+        print("Invalid option")
